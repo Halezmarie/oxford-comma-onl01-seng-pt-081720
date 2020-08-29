e61f43ecad114352c36c46d4c00e1d9a91709ef3
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-29 21:22:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,9 @@ def oxford_comma(array)
 return array[0]
   elsif array.length == 2
     return "#{array[0]} and #{array[1]}"
-    # add comma plus "and" for 3 element array
   else
     return "#{array[0..-2].join(", ")}, and #{array.last}"
+     # add comma plus "and" for 3 element array
   end
 end
 
